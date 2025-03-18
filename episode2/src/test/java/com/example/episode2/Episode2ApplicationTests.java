e7d696aadfb4a0523b0176fd81ac26a1a5ad8f4a
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-03-18 22:38:00 +0600

    init

---
+++ episode2/src/test/java/com/example/episode2/Episode2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.episode2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Episode2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
