bd7cb28d58f05d01ac5455ea99ee4239c421fb7c
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-05-10 06:41:16 +0600

    Basic Authentication Implementation

--- Code Snippet/security/src/main/java/com/example/security/config/SecurityConfig.java
+++ Code Snippet/security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -6,15 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static org.springframework.aot.generate.ValueCodeGenerator.withDefaults;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.httpBasic();
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().authenticated())
+                .httpBasic();
         return http.build();
     }
 }
