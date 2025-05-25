11d5d34041afeb71393f0689310ed7376c856b2b
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-05-25 18:18:29 +0600

    creating jpa entity and repository classes for new table

--- Eazy Bytes/section4/springsecsection4/src/main/resources/sql/sqripts.sql
+++ Eazy Bytes/section4/springsecsection4/src/main/resources/sql/sqripts.sql
@@ -6,3 +6,14 @@ INSERT IGNORE INTO `users` VALUES('user', '{noop}EazyBytes@12345', '1');
 INSERT IGNORE INTO `authorities` VALUES('user', 'read');
 INSERT IGNORE INTO `users` VALUES('admin', '{bcrypt}$2a$12$Ld7yqGzntaBJRlPiFlkWZOuvY2jEN/Dfn71VvFSWIeJ3TksELVIHC', '1');
 INSERT IGNORE INTO `authorities` VALUES('admin', 'admin');
+
+CREATE TABLE `customer`(
+    `id` int NOT NULL AUTO_INCREMENT,
+    `email` varchar(45) NOT NULL,
+    `pwd` varchar(200) NOT NULL,
+    `role` varchar(45) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+INSERT INTO `customer` (`email`, `pwd`, `role`) VALUES('happy@example.com', '{noop}EazyBytes@12345', 'read');
+INSERT INTO `customer` (`email`, `pwd`, `role`) VALUES('admin@example.com', '{bcrypt}$2a$12$Ld7yqGzntaBJRlPiFlkWZOuvY2jEN/Dfn71VvFSWIeJ3TksELVIHC', 'admin');
