06b41a1245874b48ecc96a6f7719c101a680cf42
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-05-27 16:49:03 +0600

    customizing the AuthenticationProvider

---
+++ Eazy Bytes/section6/springsecsection6/src/main/java/com/farhad/model/Customer.java
@@ -0,0 +1,23 @@
+package com.farhad.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "customer")
+@Getter @Setter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+    @Column(name = "pwd")
+    private String password;
+    @Column(name = "role")
+    private String role;
+}
