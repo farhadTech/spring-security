d36edc8ad25dc6926c8182e80e2730e421a2028d
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-05-28 14:08:00 +0600

    defining custom authentication entry point

--- Eazy Bytes/section7/springsecsection7/src/main/java/com/farhad/config/ProjectSecurityConfig.java
+++ Eazy Bytes/section7/springsecsection7/src/main/java/com/farhad/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.farhad.config;
 
+import com.farhad.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -30,7 +31,8 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/notices", "/contact", "/error", "/register").permitAll()
                 );
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         return http.build();
     }
 
--- Eazy Bytes/section7/springsecsection7/src/main/java/com/farhad/config/ProjectSecurityProdConfig.java
+++ Eazy Bytes/section7/springsecsection7/src/main/java/com/farhad/config/ProjectSecurityProdConfig.java
@@ -1,5 +1,6 @@
 package com.farhad.config;
 
+import com.farhad.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,7 +27,7 @@ public class ProjectSecurityProdConfig {
                         .requestMatchers("/notices", "/contact", "/error", "/register").permitAll()
                 );
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
         return http.build();
     }
 
