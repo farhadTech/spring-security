2eda7a060abe894f285d67690dc21620022811c5
Farhadur Rahman <fr.farhad.ur.rahman@gmail.com>
2025-05-28 22:17:22 +0600

    readme.md edited

--- Eazy Bytes/section7/springsecsection7/src/main/java/com/farhad/config/ProjectSecurityConfig.java
+++ Eazy Bytes/section7/springsecsection7/src/main/java/com/farhad/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.farhad.config;
 
+import com.farhad.exceptionhandling.CustomAccessDeniedHandler;
 import com.farhad.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,7 +33,7 @@ public class ProjectSecurityConfig {
                 );
         http.formLogin(withDefaults());
         http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
-        http.exceptionHandling(ehc -> ehc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
