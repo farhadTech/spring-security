925da73949cbea0d1294d85e9390c79c0cd13985
Farhadur Rahman <farhad.rahmandev@gmail.com>
2025-06-13 14:24:37 +0600

    request validation with custom filters

--- EmbarkX/notes/src/main/java/com/security/notes/security/SecurityConfig.java
+++ EmbarkX/notes/src/main/java/com/security/notes/security/SecurityConfig.java
@@ -8,13 +8,12 @@ import com.security.notes.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import java.time.LocalDate;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -32,6 +31,7 @@ public class SecurityConfig {
                     .anyRequest().authenticated());
 //        http.formLogin(withDefaults());
         http.csrf(csrf -> csrf.disable());
+        http.addFilterBefore(new CustomLoggingFilter(), UsernamePasswordAuthenticationFilter.class);
         http.httpBasic(withDefaults());
         return http.build();
     }
